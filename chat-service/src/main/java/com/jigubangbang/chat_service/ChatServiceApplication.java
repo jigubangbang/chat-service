9e16afe8f819502cb9756f2129b89b2e429aceb9
seolyeong333 <33qqpp@naver.com>
2025-06-17 15:36:27 +0900

    Initial Commit

---
+++ chat-service/src/main/java/com/jigubangbang/chat_service/ChatServiceApplication.java
@@ -0,0 +1,14 @@
+package com.jigubangbang.chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class ChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServiceApplication.class, args);
+	}
+
+}
